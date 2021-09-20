31901ff6f230e1124dad42ffa90c01fbbcbeb8d2
alamin500 <nalamin500@gmail.com>
2021-09-20 11:41:20 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Countries></Countries>
+    </div>
+  );
+}
+function Countries(){
+  return (
+    <div>
+      <h2>Travelling around the world!!</h2>
+    </div>
+  )
+}
+export default App;
