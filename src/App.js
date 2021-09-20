7dc7f09aa5d5a1ac97b05321f7ea6bfe5cfd5df1
alamin500 <nalamin500@gmail.com>
2021-09-20 12:46:58 +0600

    g

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-       <Countries></Countries>
+     
     </div>
   );
 }
-function Countries(){
-  return (
-    <div>
-      <h2>Travelling around the world!!</h2>
-    </div>
-  )
-}
-export default App;
+// function Countries(){
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() =>{  
+
+//     fetch('https://restcountries.eu/rest/v2/all')
+//     .then(res => res.json())
+//     .then(data => setCountries(data));
+
+//   }, [])
+//   return (
+//     <div>
+//       <h2>Travelling around the world!!</h2>
+//       <h4>Countries Available:{countries.length}</h4>
+//       {
+//         countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props){
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <p>Capital: {props.capital}</p>
+//     </div>
+//   )
+// }
+// export default App;
